af7550d240d8c043cfafb755023d6701c97cecc9
rivaelRodriguesJr <rivael.rodriguesjr@outlook.com>
2019-06-27 13:22:58 -0300

    Deleção de usuário com DELETE

--- src/main/java/br/org/fitec/workshopmongo/resorces/UserResorce.java
+++ src/main/java/br/org/fitec/workshopmongo/resorces/UserResorce.java
@@ -48,5 +48,11 @@ public class UserResorce {
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
 		return ResponseEntity.created(uri).build();
 	}
+	
+	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+	public ResponseEntity<Void> delete(@PathVariable String id) {		
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
 
 }
