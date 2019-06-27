6a94cbc94c4f801c35ca3f79d9a805efe8780873
rivaelRodriguesJr <rivael.rodriguesjr@outlook.com>
2019-06-27 12:42:27 -0300

    Inserção de usuário com POST

--- src/main/java/br/org/fitec/workshopmongo/resorces/UserResorce.java
+++ src/main/java/br/org/fitec/workshopmongo/resorces/UserResorce.java
@@ -1,14 +1,17 @@
 package br.org.fitec.workshopmongo.resorces;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import br.org.fitec.workshopmongo.domain.User;
 import br.org.fitec.workshopmongo.dto.UserDTO;
@@ -35,6 +38,15 @@ public class UserResorce {
 		User user = service.findById(id);
 		UserDTO userDto = new UserDTO(user);  
 		return ResponseEntity.ok().body(userDto);
+	}
+	
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Void> insert(@RequestBody UserDTO objDto){
+		User obj = service.fromDTO(objDto);
+		obj = service.insert(obj);
+		//comado específico do Spring que coloca no <head> o caminho do arquivo inserido
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 
 }
--- src/main/java/br/org/fitec/workshopmongo/services/UserService.java
+++ src/main/java/br/org/fitec/workshopmongo/services/UserService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import br.org.fitec.workshopmongo.domain.User;
+import br.org.fitec.workshopmongo.dto.UserDTO;
 import br.org.fitec.workshopmongo.repository.UserRepository;
 import br.org.fitec.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -23,5 +24,12 @@ public class UserService {
 	public User findById(String id) {
 		return repo.findById(id).orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
-
+	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
+	}
 }
