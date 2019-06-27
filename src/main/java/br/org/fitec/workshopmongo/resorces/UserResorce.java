74ce28020fb3695c8206a64d7247f6f09b97ad08
rivaelRodriguesJr <rivael.rodriguesjr@outlook.com>
2019-06-27 13:40:41 -0300

    Atualização de usuário com PUT

--- src/main/java/br/org/fitec/workshopmongo/resorces/UserResorce.java
+++ src/main/java/br/org/fitec/workshopmongo/resorces/UserResorce.java
@@ -54,5 +54,15 @@ public class UserResorce {
 		service.delete(id);
 		return ResponseEntity.noContent().build();
 	}
+	
+	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+	public ResponseEntity<Void> update(@RequestBody UserDTO objDto, @PathVariable String id) {		
+		User obj = service.fromDTO(objDto);
+		obj.setId(id);
+		service.update(obj);
+		return ResponseEntity.noContent().build();
+	}
+	
+	
 
 }
