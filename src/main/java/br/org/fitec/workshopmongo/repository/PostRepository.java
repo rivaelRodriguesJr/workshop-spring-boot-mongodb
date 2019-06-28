9a3504f9f034e4f418cffba5411e4525f06d09a1
rivaelRodriguesJr <rivael.rodriguesjr@outlook.com>
2019-06-28 13:06:30 -0300

    Consulta simples com @Query

--- src/main/java/br/org/fitec/workshopmongo/repository/PostRepository.java
+++ src/main/java/br/org/fitec/workshopmongo/repository/PostRepository.java
@@ -3,6 +3,7 @@ package br.org.fitec.workshopmongo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import br.org.fitec.workshopmongo.domain.Post;
@@ -12,5 +13,8 @@ import br.org.fitec.workshopmongo.domain.Post;
 // MongoRepository<T, ID> => Subinteface específica da inteface Repository para consulta em bancos Mongo
 public interface PostRepository extends MongoRepository<Post, String>{
 
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+	List<Post> searchTitle(String txt);
+	
 	List<Post> findByTitleContainingIgnoreCase(String text);
 }
